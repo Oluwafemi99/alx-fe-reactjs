2cb650cc972f20e2f8c6ec8fd2afacfe1744689f
oluwafemi <oluwafemidojumo@gmail.com>
2026-01-18 14:22:14 +0100

    counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,10 +7,9 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/counter";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div>
@@ -22,6 +21,7 @@ function App() {
 					age="25"
 					bio="Loves hiking and photography"
 				/>
+        <Counter/>
 				<Footer />
 			</div>
 			<p className="read-the-docs">
