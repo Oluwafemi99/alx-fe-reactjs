affe64dd66a039f2f26f0f1a1e29340b854f7597
oluwafemi <oluwafemidojumo@gmail.com>
2026-01-18 16:10:37 +0100

    contextAPI

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,12 +1,14 @@
 import ProfilePage from "./components/ProfilePage";
 import { UserContext } from "./components/UserContext";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-	const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+	const userData = { name: "Jane Doe", email: "jane.doe@example.com", age: '30', bio: 'A simple Person' };
 
 	return (
 		<UserContext.Provider value={userData}>
-			<ProfilePage />
+      <ProfilePage />
+      <UserProfile />
 		</UserContext.Provider>
 	);
 }
