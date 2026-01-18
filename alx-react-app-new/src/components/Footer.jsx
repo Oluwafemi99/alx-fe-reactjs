80ab8edeb0e2dc873ddcf11372df1928ec750ef2
oluwafemi <oluwafemidojumo@gmail.com>
2026-01-18 13:51:21 +0100

    inline style

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<p style={{ color: "#8da7a7", fontSize: "12px" }}>© 2023 City Lovers</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainContent() {
+	return (
+		<main
+			style={{
+				border: "solid 1px gray",
+				color: "#8da7a7",
+				fontWeight: "bold",
+				fontSize: "20px",
+			}}
+		>
+			<p>I love to visit New York, Paris, and Tokyo.</p>
+		</main>
+	);
+}
+
+export default MainContent;
