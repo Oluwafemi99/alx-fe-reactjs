7676491ebeefda47e0af9003b4435078edc0f26a
oluwafemi <oluwafemidojumo@gmail.com>
2026-01-25 21:35:21 +0100

    recipe sharing app created

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import useRecipeStore from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 function DeleteRecipeButton(recipeId) {
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const navigate = useNavigate();
+
     
 	const handleDelete = () => {
 		if (window.confirm("Are you sure you want to delete this recipe?")) {
-			deleteRecipe(recipeId);
+            deleteRecipe(recipeId);
+            // optional: redirect to home after deletion
+            navigate("/");
+
 		}
 	};
 	return (
