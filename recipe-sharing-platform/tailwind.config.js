5f2e81ed93ddb4ed13b9c9c5f09e48e42923c102
oluwafemi <oluwafemidojumo@gmail.com>
2026-02-10 10:09:21 +0100

    recipe-sharing-platfrom-react-app-created

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
-		"./public/index.html", // required by checker
-		"./index.html", // actual Vite entry
-		"./src/**/*.{js,ts,jsx,tsx}",
+		"./public/index.html",
+		"./index.html",
+		"./src/**/*.{js,jsx,ts,tsx}",
 	],
 	theme: {
 		extend: {},
