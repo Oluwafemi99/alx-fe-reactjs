7a08d7b943d2744e2466feed7fa36af816821ee2
oluwafemi <oluwafemidojumo@gmail.com>
2026-02-22 21:55:35 +0100

    react-router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,28 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileSettings from "../pages/ProfileSettings";
+import ProfileDetails from "../pages/ProfileDetails";
 
 function Profile() {
 	return (
 		<div>
 			<h1>Profile Page</h1>
+
 			<nav>
 				<Link to="details">Profile Details</Link> |{" "}
 				<Link to="settings">Profile Settings</Link>
 			</nav>
-			<Outlet /> {/* Nested routes render here */}
+
+			{/* Nested Routes defined here */}
+			<Routes>
+				<Route
+					path="details"
+					element={<ProfileDetails />}
+				/>
+				<Route
+					path="settings"
+					element={<ProfileSettings />}
+				/>
+			</Routes>
 		</div>
 	);
 }
